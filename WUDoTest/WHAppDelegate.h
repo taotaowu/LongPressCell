c74183638bb4bd4c6d8aed22696c4ce1c6a14f84
haitaowu <whatanost@gmail.com>
2014-06-15 20:43:28 +0800

    Initial Commit

---
+++ WUDoTest/WHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WHAppDelegate.h
+//  WUDoTest
+//
+//  Created by 吴海涛 on 14-6-15.
+//  Copyright (c) 2014年 吴海涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WUDoTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WUDoTest
+//
+//  Created by 吴海涛 on 14-6-15.
+//  Copyright (c) 2014年 吴海涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WHAppDelegate class]));
+    }
+}
